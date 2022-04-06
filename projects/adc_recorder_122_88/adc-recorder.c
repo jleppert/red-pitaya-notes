1e7c6b763874e67e85fce598d653c325f9495dcd
Johnathan Leppert <johnathan@inspectobot.com>
2022-04-06 04:49:42 +0000

    implement continuous sweep and single sweep trigger mode

--- projects/adc_recorder_122_88/adc-recorder.c
+++ projects/adc_recorder_122_88/adc-recorder.c
@@ -38,7 +38,69 @@ void setFrequency(int frequency, int intermediateFrequency) {
   rfSource->Write("f" + std::to_string(frequency + intermediateFrequency));
 }
 
-// 4096 bytes per page
+void enableExcitation(int transmitPower, int loPower) {
+  rfSource->Write("C0");
+  rfSource->Write("W" + std::to_string(transmitPower));
+  rfSource->Write("C1");
+  rfSource->Write("W" + std::to_string(loPower));
+
+  rfSource->Write("C0");
+
+  rfSource->Write("E1");
+  rfSource->Write("r1");
+
+  rfSource->Write("C1");
+
+  rfSource->Write("E1");
+  rfSource->Write("r1");
+}
+
+void disableExcitation() {
+  rfSource->Write("C0");
+
+  rfSource->Write("E0");
+  rfSource->Write("r0");
+
+  rfSource->Write("C1");
+
+  rfSource->Write("E0");
+  rfSource->Write("r0");
+}
+
+void setupSweep(
+  int startFrequency, 
+  int stepFrequency, 
+  int frequencyCount, 
+  int intermediateFrequency,
+  float stepTimeInMs) {
+
+  rfSource->Write("w1");
+
+  rfSource->Write("l" + std::to_string(startFrequency));
+
+  rfSource->Write("u" + std::to_string(startFrequency + (stepFrequency * frequencyCount)));
+
+  rfSource->Write("t" + std::to_string(stepTimeInMs));
+
+  rfSource->Write("k" + std::to_string(intermediateFrequency));
+
+  rfSource->Write("n2");
+
+  rfSource->Write("X0");
+
+  rfSource->Write("^1");
+
+  rfSource->Write("c0");
+}
+
+void runContinuousSweep() {
+  rfSource->Write("c1");
+  rfSource->Write("g1");
+}
+
+void runSingleSweep() {
+  rfSource->Write("g1");
+}
 
 int main() {
   int startFrequency   = 1000;
@@ -47,32 +109,30 @@ int main() {
   int intermediateFreq = 32;
   int transmitPower    = 0;
   int loPower          = 15;
+  float stepTimeInMs   = 0.5;
   uint32_t sampleCount = 1024;
 
   long long int sampleTimeInNs = (1 / ADC_SAMPLE_RATE) * sampleCount * 1000000000;
 
-  //uint32_t settlingSamples = ADC_SAMPLE_RATE * 0.001;
-  
+  printf("Sample count: %lu\n", (unsigned long)sampleCount); 
+  printf("Sample time in nanoseconds: %llu", sampleTimeInNs);
+
   rfSource = new SerialPort("/dev/ttyACM0", BaudRate::B_57600, NumDataBits::EIGHT, Parity::NONE, NumStopBits::ONE);
   rfSource->SetTimeout(0);
   rfSource->Open();
 
-  rfSource->Write("C0");
-  rfSource->Write("W" + std::to_string(transmitPower));
-  rfSource->Write("C1");
-  rfSource->Write("W" + std::to_string(loPower));
-
+  setupSweep(startFrequency, stepFrequency, frequencyCount, intermediateFreq, stepTimeInMs);
   setFrequency(startFrequency, intermediateFreq);
 
-  rfSource->Write("C0");
+  enableExcitation(transmitPower, loPower);
 
-  rfSource->Write("E1");
-  rfSource->Write("r1");
+  runContinuousSweep();
 
-  rfSource->Write("C1");
+  sleep(10);
 
-  rfSource->Write("E1");
-  rfSource->Write("r1");
+  disableExcitation();
+
+  exit(0);
 
   int fd, i;
   volatile uint8_t *rst;
@@ -95,8 +155,10 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  // rounded up to nearest page
+  // rounded up to nearest page (4096 bytes per page)
   size = ceil( (sampleCount * BYTES_PER_SAMPLE * frequencyCount * CHANNEL_COUNT) / sysconf(_SC_PAGESIZE) ) * sysconf(_SC_PAGESIZE);
+  
+  printf("Allocated size: %lu\n", (unsigned long)size); 
 
   if(ioctl(fd, CMA_ALLOC, &size) < 0) {
     perror("ioctl");
@@ -111,40 +173,36 @@ int main() {
   *(uint32_t *)(cfg + 8) = sampleCount;
 
   int64_t startTime = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+  while(1) {
+    for(int i = 0; i < frequencyCount; i++) {
+      // set writer address
+      //*(uint32_t *)(cfg + 4) = samplecount * bytes_per_sample * channel_count * i;
+      
+      *(uint32_t *)(cfg + 4) = size;
+      *(uint32_t *)(cfg + 8) = sampleCount - 1;
 
-  for(int i = 0; i < frequencyCount; i++) {
-    // set writer address
-    *(uint32_t *)(cfg + 4) = sampleCount * BYTES_PER_SAMPLE * CHANNEL_COUNT * i;
+      // reset writer
+      *rst &= ~4;
+      *rst |= 4;
 
-    // reset writer
-    *rst &= ~4;
-    *rst |= 4;
+      // reset packetizer
+      *rst &= ~2;
+      *rst |= 2;
 
-    // reset packetizer
-    *rst &= ~2;
-    *rst |= 2;
+      // wait for sample
+      std::this_thread::sleep_for(std::chrono::nanoseconds(sampleTimeInNs));
 
-    // wait for sample
-    std::this_thread::sleep_for(std::chrono::nanoseconds(sampleTimeInNs));
+      setFrequency(startFrequency + (i * stepFrequency), intermediateFreq);
 
-    setFrequency(startFrequency + (i * stepFrequency), intermediateFreq);
+      std::this_thread::sleep_for(std::chrono::microseconds(100));
+    }
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(1));
+    int64_t endTime = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+
+    printf("Sweep Done, took %lld microseconds\n", endTime - startTime);
   }
 
-  int64_t endTime = duration_cast<microseconds>(system_clock::now().time_since_epoch()).count();
+  disableExcitation();
 
-  printf("Sweep Done, took %lld microseconds\n", endTime - startTime);
-
-  rfSource->Write("C0");
-
-  rfSource->Write("E0");
-  rfSource->Write("r0");
-
-  rfSource->Write("C1");
-
-  rfSource->Write("E0");
-  rfSource->Write("r0");
-
-  return EXIT_SUCCESS;
+  exit(-1);
 }
--- projects/adc_recorder_122_88/start.sh
+++ projects/adc_recorder_122_88/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat /root/devel/red-pitaya-notes/projects/adc_recorder_122_88.bit > /dev/xdevcfg
+cat ./adc_recorder_122_88.bit > /dev/xdevcfg
