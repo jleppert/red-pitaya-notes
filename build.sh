1248f8bf5b8de0660c072abf1d2673462dd06935
Johnathan Leppert <johnathan@flyvoly.com>
2022-04-25 13:41:27 -0700

    update build script

--- build.sh
+++ build.sh
@@ -2,4 +2,8 @@
 
 # vivado -source scripts/project.tcl -tclargs adc_recorder_122_88 xc7z020clg400-1 
 
+make PART=xc7z020clg400-1 boot.bin
+make PART=xc7z020clg400-1 uImage
+make PART=xc7z020clg400-1 devicetree.dtb
+sudo sh scripts/image.sh scripts/ubuntu.sh red-pitaya-ubuntu-20.04-armhf.img 1024
 make NAME=adc_recorder_122_88 PART=xc7z020clg400-1 bit
